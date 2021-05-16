cfd83da9f83f5dc7a0ad0671d1621d8b5c839dfa
ajanthan <ajanthan.kt@gmail.com>
2021-05-16 22:34:37 +1000

    Create Unit / Integration test
    Create Unit /Integration Test.
    Use InMemoryDatabase to mock data setup.
    Test service class which implement create update delete search functionality.

---
+++ tests/BookCatelogueServiceTest.cs
@@ -0,0 +1,76 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using AutoMapper;
+using BookCatalogue.Application.BookRegistration.Commands;
+using BookCatalogue.Application.BookRegistration.Query;
+using BookCatalogue.Application.Command;
+using BookCatalogue.Application.Dto;
+using BookCatalogue.Application.Enums;
+using BookCatalogue.Application.Services;
+using BookCatalogue.Tests.Persistence;
+using BookCatalogue.Tests.Database;
+
+namespace BookCatalogue.Tests
+{
+    public class BookCatelogueServiceTest
+    {
+        readonly IBookCatelogueService sut;
+        readonly IMapper mapper;
+
+        public BookCatelogueServiceTest(IBookCatelogueService bookCatelogueService, IMapper mapper)
+        {
+            this.sut = bookCatelogueService;
+            this.mapper = mapper;
+        }
+
+        [Theory]
+        [ClassData(typeof(CreateBookCatelogData))]
+        public async Task Create_BookCatalogue_ReturnsCreatedStatus(Status expected, CreateBookCatalogueCommand createBookCatalogueCommand)
+        {
+
+            var response = await sut.CreateBookCatalogue(createBookCatalogueCommand);
+            Assert.Equal(expected, response.Status);
+        }
+
+        [Theory]
+        [ClassData(typeof(UpdateBookCatelogData))]
+        public async Task Update_BookCatalogue_ReturnsCreatedStatus(Status expected, UpdateBookCatalogueCommand bookcatelog)
+        {
+            var response = await sut.UpdateBookCatalogue(bookcatelog);
+            var bookCatelog = sut.GetBookCatalogue(bookcatelog.Id);
+            Assert.True(response.Status == expected && bookCatelog.bookCatalogues.FirstOrDefault().ISBN== bookcatelog.ISBN);
+        }
+
+        [Theory]
+        [ClassData(typeof(GetSearchCatalogueQueryData))]
+        public void Get_SearchCatalogue_QueryData(SearchCatalogueResponse expected, GetSearchCatalogueQuery getSearchCatalogueQuery)
+        {
+            var response =  sut.GetBookCatalogues(getSearchCatalogueQuery);
+            Assert.Equal(expected.Status, response.Status);
+        }
+
+        [Fact]
+        public void Get_BookCatalogueById_ReturnsSuccessWithResponse()
+        {
+            var response =  sut.GetBookCatalogue(DbContextSetup.BookIdSample2Id);
+            Assert.True(response.Status == Status.Success && response.bookCatalogues.FirstOrDefault().Id  == DbContextSetup.BookIdSample2Id);
+        }
+
+        [Fact]
+        public void Verify_BookCatalogueExists_ReturnExistance()
+        {
+            var response = sut.BookExists(DbContextSetup.BookIdSample2Id);
+            Assert.True(response); 
+        }
+
+        [Fact]
+        public async Task Verify_RemoveBookCatalogue_ReturnExistance()
+        {
+            var existing = sut.BookExists(DbContextSetup.BookIdSample3Id);
+            var response = await sut.DeleteBookCatalogue(DbContextSetup.BookIdSample3Id);
+            var deleted = !(sut.BookExists(DbContextSetup.BookIdSample3Id));
+            Assert.True(existing && response.Status == Status.Success && deleted);
+        }
+    }
+}
---
+++ tests/Persistence/DbContextSetup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using BookCatalogue.Domain.Entities;
+
+namespace BookCatalogue.Tests.Database
+{
+    public static class DbContextSetup
+    {
+        public static Guid BookIdSample1Id { get; set; } = Guid.Parse("da88f3e4-0fb7-48da-8c4c-4403df4d0ef3");
+
+        public static List<Author> AuthorsBook1 = new List<Author>()
+                                {
+                                    new Author() { AuthorName = "AJANTHAN", Id = Guid.NewGuid() },
+                                    new Author() { AuthorName = "Kajotha", Id = Guid.NewGuid() }
+                                };
+        public static Guid BookIdSample2Id { get; set; } = Guid.Parse("afd39f21-c335-499e-bdf4-b7aa194f1126");
+
+        public static List<Author> AuthorsBook2 = new List<Author>()
+                                {
+                                    new Author() { AuthorName = "Steve", Id = Guid.NewGuid() }
+                                };
+        public static Guid BookIdSample3Id { get; set; } = Guid.Parse("ac852424-f6ea-4c29-bd1f-b8fb3b45c21f");
+
+        public static List<Author> AuthorsBook3 = new List<Author>()
+                                {
+                                    new Author() { AuthorName = "Mark", Id = Guid.NewGuid() }
+                                };
+
+        public static List<Book> Books { get; set; } = SetupBooks();
+        private static List<Book> SetupBooks()
+        {
+            List<Book> Books = new List<Book>();
+            Books.Add(
+                        new Book()
+                        {
+                            Id = BookIdSample1Id,
+                            ISBN = "1234567890123",                           
+                            Title = "Sample One",
+                            PublicationDate = DateTime.UtcNow,
+                            Authors = AuthorsBook1
+                        });
+            Books.Add(
+                      new Book()
+                      {
+                          Id = BookIdSample2Id,
+                          ISBN = "0456667778992",
+                          Title = "Second Book",
+                          PublicationDate = DateTime.UtcNow,
+                          Authors = AuthorsBook2
+                      });
+            Books.Add(
+                    new Book()
+                    {
+                        Id = BookIdSample3Id,
+                        ISBN = "9993334443332",
+                        Title = "Thrid Book",
+                        PublicationDate = DateTime.UtcNow,
+                        Authors = AuthorsBook3
+                    });
+
+
+            return Books;
+        }
+
+      
+
+    }
+}
