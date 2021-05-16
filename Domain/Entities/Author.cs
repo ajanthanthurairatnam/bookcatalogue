23771365308a4ad8079470feb881c30bbe27627f
ajanthan <ajanthan.kt@gmail.com>
2021-05-16 22:17:36 +1000

    Create Domain Layer
    Create Domain Layer
    Generate Domain Classes

---
+++ Domain/Entities/Author.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BookCatalogue.Domain.Entities
+{
+    public class Author
+    {
+        public Guid Id { get; set; }
+        public string AuthorName { get; set; }
+        public Book Book { get; set; }
+    }
+}
