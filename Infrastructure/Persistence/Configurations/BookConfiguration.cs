1572c48472e70c8cf1c6104fbb4e51ac6b76c7f3
ajanthan <ajanthan.kt@gmail.com>
2021-05-16 22:29:37 +1000

    Create Infrastructure Layer
    Implement Interaction with Database using EFCore.

---
+++ Infrastructure/Persistence/Configurations/BookConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using BookCatalogue.Domain.Entities;
+
+namespace BookCatalogue.Infrastructure.Persistence.Configurations
+{
+    public class BookConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.Property(t => t.ISBN)
+                  .HasColumnType("varchar")
+                  .HasMaxLength(13)
+                  .IsRequired();
+
+            builder.Property(t => t.Title)
+                .HasColumnType("varchar")
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(t => t.PublicationDate)
+                .HasColumnType("datetime")
+                .IsRequired();
+
+            builder.HasMany(t=>t.Authors).WithOne(e=>e.Book);
+
+        }
+    }
+}
